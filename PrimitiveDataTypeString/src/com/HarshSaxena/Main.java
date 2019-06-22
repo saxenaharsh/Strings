243f7741b71f5b175873f5d15c89a6d82b84b756
saxenaharsh <harsh.saxena@gmail.com>
2019-06-22 12:23:29 +0530

    Add files via upload

---
+++ PrimitiveDataTypeString/src/com/HarshSaxena/Main.java
@@ -0,0 +1,30 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        /*
+         * byte
+         * short
+         * int
+         * long
+         * float
+         * double
+         * char
+         * boolean
+         *
+         *
+         * */
+
+        String myString = "This is s string";
+        System.out.println(myString);
+
+        myString = myString + " and that's how we use a String variable";
+        System.out.println(myString);
+
+        String numberString = "250.55";
+        numberString = numberString + " 49.95";
+        System.out.println("The value of numberString is " + numberString);
+    }
+}
